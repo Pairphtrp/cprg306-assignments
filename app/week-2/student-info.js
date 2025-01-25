a550c5805429a0cdf79f5f3c6f377b4062aec8e1
Pairphtrp <phattharaporniamcharoen@gmail.com>
2025-01-24 20:24:11 -0700

    week-2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+export default function StudentInfo() {
+    return (
+      <main>
+        <h1>Phattharaporn Iamcharoen(Pear)</h1>
+        <h2>
+        <a href="https://github.com/Pairphtrp" target="_blank" rel="noopener noreferrer">
+          GitHub
+        </a>
+      </h2>
+      </main>
+    );
+  }
