e9876a8e394089b2bbbfd866d5f65bb2fe03cfc1
Pairphtrp <phattharaporniamcharoen@gmail.com>
2025-02-07 20:01:53 -0700

    week-4 edit

---
+++ app/week-1/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+      <div>
+        <h1>Week 1</h1>
+        <p>Phattharaporn Iamcharoen 000932042</p>
+      </div>
+    );
+  };
+
+export default Page;
